3f66749b69953ffbc50c244ba85ccee1e0eb5e7b
Kai Franke <cosinushh@gmail.com>
2023-01-06 13:20:45 +0100

    fetch bootcamp data from API and set state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import BootcampCard from "./components/BootcampCard/BootcampCard.js";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [allBootcamps, setAllBootcamps] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/bootcamps/");
+        const json = await response.json();
+        setAllBootcamps(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log("allBootcamps", allBootcamps);
+
+  return (
+    <div className="App">
+      <main>
+        <BootcampCard />
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/BootcampCard/BootcampCard.js
@@ -0,0 +1,20 @@
+import "./BootcampCard.css";
+
+export default function BootcampCard() {
+  return (
+    <>
+      <h2>Bootcamp Name</h2>
+      <h3>Instructors:</h3>
+      <ul>
+        <li>Name 1</li>
+        <li>Name 2</li>
+      </ul>
+      <h3>Developers:</h3>
+      <ul>
+        <li>Name 1</li>
+        <li>Name 2</li>
+        <li>Name 3</li>
+      </ul>
+    </>
+  );
+}
